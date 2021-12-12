e2692947bfdb0a251bfcd9e6d4e255db3d202c42
Yash Sonalia <yashsonalia@credenceanalytics.com>
2021-12-12 14:45:07 +0530

    Heroku test run

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./scss/index.scss";
+import "./css/index.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
